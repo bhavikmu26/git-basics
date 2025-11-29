51582c71922fde18440c920e07a6fc1f32343086
Bhavik <bhavikmunot@rediffmail.com>
2025-11-29 18:36:09 +0530

    Iniitial Git commit to understand git workflow

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("This is a git basics repo");
+        System.out.println( "Hello World!" );
+    }
+
+
+}
