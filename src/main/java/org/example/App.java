1a177ec341d10a02aaaab54b0d8d40f2257a22e8
Bhavik <bhavikmunot@rediffmail.com>
2025-11-29 18:44:41 +0530

    Commit changes without staging

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,16 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("This is a git basics repo");
+
+
+
+
+
+
+        //Test comment
+        for(int i=0; i<5; i++) {
+            System.out.println("Iteration: " + i);
+        }
         System.out.println( "Hello World!" );
     }
 
